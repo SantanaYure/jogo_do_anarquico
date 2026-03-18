38473c267d4584197f25a580f2e6e145b395dc5f
yure.s.santana@outlook.com <yure.s.santana@outlook.com>
2026-03-17 21:26:23 -0300

    refactor: simplify bot initialization and remove unused interaction handling

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
+// require("dotenv").config();
+// const { Client, GatewayIntentBits } = require("discord.js");
+
+// const client = new Client({
+//   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages]
+// });
+
+// client.once("ready", () => {
+//   console.log("Bot online!");
+// });
+
+// client.on("interactionCreate", async interaction => {
+//   if (!interaction.isChatInputCommand()) return;
+
+//   const { commandName } = interaction;
+
+//   const command = require(`./commands/${commandName}.js`);
+//   command.execute(interaction);
+// });
+
+// client.login(process.env.TOKEN);
+
 require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
 
+console.log("Iniciando bot...");
+
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages]
+  intents: [GatewayIntentBits.Guilds]
 });
 
 client.once("ready", () => {
   console.log("Bot online!");
-});
-
-client.on("interactionCreate", async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const { commandName } = interaction;
-
-  const command = require(`./commands/${commandName}.js`);
-  command.execute(interaction);
 });
 
 client.login(process.env.TOKEN);
